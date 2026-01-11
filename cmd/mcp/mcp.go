508e232e0b8c55079c131d6fae3ca26c73ff3786
James Lee <jamesl33info@gmail.com>
2026-01-11 20:50:15 +0000

    Improve tool descriptions

--- cmd/mcp/mcp.go
+++ cmd/mcp/mcp.go
@@ -3,6 +3,7 @@ package mcp
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/jamesl33/zk/cmd/mcp/tools"
 	"github.com/modelcontextprotocol/go-sdk/mcp"
@@ -45,33 +46,88 @@ func (m *MCP) Run(ctx context.Context) error {
 
 	server := mcp.NewServer(&impl, nil)
 
+	description := `
+List notes in a directory.
+
+This tool returns all the notes within a directory so is therefore useful when you want to obtain *all* the notes relating to a topic.
+
+There's no filtering, so it's best for obtaining all the notes you wish to read.
+`
+
 	mcp.AddTool(
 		server,
-		&mcp.Tool{Name: "list_notes", Description: "List notes in a directory"},
+		&mcp.Tool{Name: "list_notes", Description: strings.TrimSpace(description)},
 		tools.ListNotes,
 	)
 
+	description = `
+Search the title, tags and content of notes using a regular expression.
+
+It's a case sensitive, multi-line search by default; to make it case insensive, use "(?i:$EXPRESSION)".
+
+This is an excellent way to find notes, it should be used when:
+
+	- Searching for a specific title
+	- Searching for a tag, which is a lower case, snake case identifier (e.g. linux, thru_hiking)
+	- Searching for words or phrases which may be within a note
+
+The full note content isn't returned, you must read it using 'read_note'.
+`
+
 	mcp.AddTool(
 		server,
-		&mcp.Tool{Name: "regex_search_notes", Description: "Search note titles, tags and body using a regular expression"},
+		&mcp.Tool{Name: "regex_search_notes", Description: strings.TrimSpace(description)},
 		tools.RegexSearchNotes,
 	)
 
+	description = `
+Search for notes similar to the given phrase, topic or idea.
+
+This is an excellent way to find notes, it should be used when:
+
+	- Searching for notes which are similar, or related to a topic (but perhaps don't mention it directly)
+
+The full note content isn't returned, you must read it using 'read_note'.
+`
+
 	mcp.AddTool(
 		server,
-		&mcp.Tool{Name: "semantic_search_notes", Description: "Search for notes which are semantically similar to a query, phrase or word"},
+		&mcp.Tool{Name: "semantic_search_notes", Description: strings.TrimSpace(description)},
 		tools.SemanticSearchNotes,
 	)
 
-	mcp.AddTool(
-		server,
-		&mcp.Tool{Name: "find_related_notes", Description: "Performs a semantic search, to find related notes"},
-		tools.FindRelatedNotes,
-	)
+	description = `
+Find notes that contain information that's semantically similar or link to/from a given note.
+
+This is a great way to increase the amount of context you have before performing a task, it should be used when:
+
+	- You have a note and you'd like to find other notes that are similar (vector search)
+	- You have a note and you'd like to find other notes that are linked to/from the note (direct mentions)
+	- You have a 'bibliographic' note, and wish to find quotes/citations from the book
+
+The full note content isn't returned, you must read it using 'read_note'.
+	`
 
 	mcp.AddTool(
 		server,
-		&mcp.Tool{Name: "read_note", Description: "Read a note from disk"},
+		&mcp.Tool{Name: "find_related_notes", Description: strings.TrimSpace(description)},
+		tools.FindRelatedNotes,
+	)
+
+	description = `
+Read a note from disk.
+
+You should use this tool when:
+
+	- You have a note, and the title, tags or path don't contain enough information to perform your task
+	- You want to improve your understanding on a topic/idea, the note content is almost always required
+
+The full note is returned.
+	`
+
+	mcp.AddTool(
+		server,
+		&mcp.Tool{Name: "read_note", Description: strings.TrimSpace(description)},
 		tools.ReadNote,
 	)
 
